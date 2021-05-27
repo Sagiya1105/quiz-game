40ce4fa68aa591ffa84ba82cbc33e469fc6ce246
Sagiya1105 <81526245+Sagiya1105@users.noreply.github.com>
2021-05-27 20:01:57 +0300

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,95 @@
+let data = [];
+let score = Number;
+
+var navigation = new Vue({
+    el: '#navigation',
+    data: {
+        answers: [],
+        questionIdx: 0,
+     //   score: 0,
+    },
+    computed: {
+        isPrevDisabled: function() {
+            return this.questionIdx == 0;
+        },
+        isNextDisabled: function () {
+            return this.questionIdx == data.length - 1;
+        }
+        
+    },
+    methods: {
+        nextQuestion: () => {
+            navigation.questionIdx++;
+            showQuestion(navigation.questionIdx);
+        },
+        prevQuestion: () => {
+            navigation.questionIdx--;
+            showQuestion(navigation.questionIdx);
+        },
+        finish: () => {
+            calcScores();
+        }
+
+    }
+
+})
+
+var player = new Vue ({
+
+    el: 'player',
+    data:{
+        name: String,
+        password: String,
+        totalScore: Number
+    }
+
+})
+var question = new Vue({
+    el: '#question',
+    data: {
+        text: '',
+        answers: [],
+        picked: null,
+        correct: Number,
+    },
+    watch: {
+        picked: val => {
+            data[navigation.questionIdx].picked = val;
+        }
+    }
+
+});
+
+const showQuestion = (idx) => {
+    question.text = data[idx].question;
+    question.answers = data[idx].answers;
+    question.picked = data[idx].picked;
+    question.correct = data[idx].correct;
+}
+
+
+const startGame = (name,password) => {
+    player.name = name;
+    player.password = password;
+}
+
+const calcScores =  () => {
+    score = 0;
+    for (let i = 0; i < data.length; i++) { 
+    if (data[i].picked == data[i].correct) {
+        score++;
+   } }
+    document.write(score+" out of "+data.length + " questions ");
+
+}
+
+const init = async () => {
+    const res = await axios.get('/api/questions');
+    data = res.data.questions;
+    showQuestion(0);
+    //showCorrect(0);
+   // alert(question.correct);
+}
+
+init();
+
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const db = require('./server/db');
+
+const run = async () => {
+    await db.connect();
+    
+    app.use('/', express.static(__dirname + '/public'));
+    
+    app.get('/api/questions', async (req, res) => {
+        const questions = await db.getQuestions();
+        res.send({questions})
+    })
+
+    app.get('/api/players', async (req, res) => {
+      const questions = await db.getPlayers();
+      res.send({players})
+  })
+    
+    app.listen(port, () => {
+      console.log(`Example app listening at ${port}`);
+    });
+
+    // db.addQuestion({
+    //     question: '12+2',
+    //     answers:[43,112, 14,56],
+    //     correct: 2
+    // })
+}
+
+run();
